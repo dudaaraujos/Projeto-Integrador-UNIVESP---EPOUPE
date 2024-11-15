72100e3cfb54f131f382024f8e5928247996eced
dudaaraujos <dudaraujos@gmail.com>
2024-11-15 11:00:11 -0300

    Add files via upload

--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -2,15 +2,40 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Listas } from '../listas';
 import { listaService } from '../lista.service';
 import { Router } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrl: './lista.component.css'
+  styleUrl: './lista.component.css',
+  styles: [`
+    :host ::ng-deep .p-button {
+        margin: 0 .5rem 0 0;
+        min-width: 10rem;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    .confirmation-content {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    :host ::ng-deep .p-dialog .p-button {
+        min-width: 6rem;
+    }
+`]
 })
 export class ListaComponent implements OnInit{
+showDialog() {
+throw new Error('Method not implemented.');
+}
+  primengConfig: any;
 
-  constructor(private _listaService: listaService, private router: Router){ this.listas = [] }
+  constructor(private _listaService: listaService, private router: Router, primengConfig: PrimeNGConfig){ this.listas = [] }
 
   //@ViewChild('result') myElement!:ElementRef<HTMLLabelElement>;
   //teste(){
@@ -48,5 +73,26 @@ export class ListaComponent implements OnInit{
         }
       )
   }
+
+    displayBasic: boolean = false;
+
+    displayMaximizable: boolean = false;
+
+    displayPosition: boolean = false;
+
+    position!: string;
+
+    showBasicDialog() {
+        this.displayBasic = true;
+    }
+
+    showMaximizableDialog() {
+        this.displayMaximizable = true;
+    }
+
+    showPositionDialog(position: string) {
+        this.position = position;
+        this.displayPosition = true;
+    }
 }
 
